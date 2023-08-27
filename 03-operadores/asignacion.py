02e0ff334889ae556236d4be1751003260eb205f
mijailpc <mijail86@gmail.com>
2023-08-26 22:19:28 -0500

    entrada y salida

---
+++ 03-operadores/asignacion.py
@@ -0,0 +1,23 @@
+#Operadores de asignacion
+edad = 55
+
+print(edad)
+
+edad += 5
+edad = edad + 5
+edad -= 5
+print(edad)
+
+#operadores de incremento y decremento
+year = 2021 
+
+year = year + 1
+print(year)
+
+year = year - 1
+print(year)
+
+#pre incremento
+year = 1 + year
+print(year)
+#pre decremento
