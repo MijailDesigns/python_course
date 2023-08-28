8f96b0d8a53ebae9744fe134ec460593d89a7040
mijailpc <mijail86@gmail.com>
2023-08-28 10:27:43 -0500

    ejercicio 4

---
+++ 07-ejercicios/ejercicio4.py
@@ -0,0 +1,12 @@
+numero1 = int(input("Introducir primer numero: "))
+numero2 = int(input("Introducir segundo numero: "))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+print(f"{numero1} + {numero2} = {suma}")
+print(f"{numero1} - {numero2} = {resta}")
+print(f"{numero1} * {numero2} = {multiplicacion}")
+print(f"{numero1} / {numero2} = {division}")
