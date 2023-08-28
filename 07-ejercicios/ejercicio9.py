900e11f664ac6fe5ccb57bd45e69d6aa53d41ffc
mijailpc <mijail86@gmail.com>
2023-08-28 11:14:46 -0500

    ejercicio 10

---
+++ 07-ejercicios/ejercicio9.py
@@ -0,0 +1,8 @@
+
+
+numero = int(input("Introducir numero: "))
+
+while numero != 111:
+    print(f"Numero introducido es {numero}")
+    numero = int(input("Introduce  otro numero: "))
+print("Fin")
