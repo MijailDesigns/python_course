95dfb6c2c242e61f8eb38835838c3ee64cad4495
pc <mijail86@gmail.com>
2023-08-30 11:32:05 -0500

    modulo de datetime

--- 12-modulos/main.py
+++ 12-modulos/main.py
@@ -4,3 +4,21 @@ from mimodulo import *
 
 print(holaMundo("Mijail"))
 print(calculadora(3,5,True))
+
+#modulo de fechas
+import datetime
+
+print(datetime.date.today())
+
+fecha_completa = datetime.datetime.now()
+print(fecha_completa)
+print(fecha_completa.year)
+print(fecha_completa.month)
+print(fecha_completa.day)
+print(fecha_completa.hour)
+
+fecha_personalizada = fecha_completa.strftime("%d/%m/%Y, %H:%M:%S")
+print(fecha_personalizada)
+
+print(datetime.datetime.now().timestamp())
+print(datetime.datetime.now().time())
