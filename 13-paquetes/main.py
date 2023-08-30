932c9755e37b34b9f3c03d176ac349a9d8218baa
pc <mijail86@gmail.com>
2023-08-30 15:55:38 -0500

    paquetes

---
+++ 13-paquetes/main.py
@@ -0,0 +1,8 @@
+print("Probando paquetes")
+
+# from pack import pruebas
+# from pack import herramientas
+from pack import pruebas, herramientas
+
+pruebas.probando()
+herramientas.nombreCompleto("Peter", "Parker")
