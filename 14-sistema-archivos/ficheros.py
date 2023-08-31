8c995090fd3b35b958ae97eb55465b352e915846
pc <mijail86@gmail.com>
2023-08-30 22:35:30 -0500

    leer archivos

--- 14-sistema-archivos/ficheros.py
+++ 14-sistema-archivos/ficheros.py
@@ -13,3 +13,22 @@ archivo.write("Hola Mundo\n")
 #Cerrar archivo
 
 archivo.close()
+
+#Abrir un archivo
+ruta = str(pathlib.Path().absolute()) + "/fichero_texto.txt"
+
+archivo_lectura = open(ruta, "r")
+
+#Leer contenido
+# contenido = archivo_lectura.read()
+# print(contenido)
+
+#Leer contenido y guardarlo en lista
+lista = archivo_lectura.readlines()
+archivo_lectura.close()
+
+for elem in lista:
+    # lista_frase = elem.split()
+    # print(lista_frase)
+    # print("- "+elem.upper())
+     print("- "+elem.center(50))
