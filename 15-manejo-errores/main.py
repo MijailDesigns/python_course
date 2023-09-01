cad4d510a16b8940f54a323f1403498baca0d6c3
pc <mijail86@gmail.com>
2023-08-31 22:30:26 -0500

    multiples errores

--- 15-manejo-errores/main.py
+++ 15-manejo-errores/main.py
@@ -1,15 +1,27 @@
 #Capturar excepciones y manejar errores en codigo 
 #susceptible a fallos/errores
 
-try:
-    nombre = input("Cual es tu nombre?: ")
-    if len(nombre) > 1:
-        nombre_usuario = "Elnombre es " + nombre
-    print(nombre_usuario)
-except:
-    print("Ha ocurrido un error, mete bien el nombre")
-else:
-    print("Todo ha funcionado correctamente")
-finally:
-    print("Finalizado")
+# try:
+#     nombre = input("Cual es tu nombre?: ")
+#     if len(nombre) > 1:
+#         nombre_usuario = "Elnombre es " + nombre
+#     print(nombre_usuario)
+# except:
+#     print("Ha ocurrido un error, mete bien el nombre")
+# else:
+#     print("Todo ha funcionado correctamente")
+# finally:
+#     print("Finalizado")
 
+#Multiples excepciones
+
+try:
+    numero = int(input("dime el numero para elevarlo al cuadrado: "))
+    print("El cuadrado es "+ str(numero*numero))
+except TypeError:
+    print("Debes convertir tus cadenas a enteros en el codigo")
+except ValueError:
+    print("Introduce un numero correcto")
+except Exception as error:
+    print(type(error))
+    print(f"Ha ocurrido un error: {type(error).__name__}")
