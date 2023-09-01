68d4222fbd0f2e12185094f918e912b0e42689fd
pc <mijail86@gmail.com>
2023-08-31 22:38:06 -0500

    excepciones personalizadas

--- 15-manejo-errores/main.py
+++ 15-manejo-errores/main.py
@@ -15,13 +15,29 @@
 
 #Multiples excepciones
 
+# try:
+#     numero = int(input("dime el numero para elevarlo al cuadrado: "))
+#     print("El cuadrado es "+ str(numero*numero))
+# except TypeError:
+#     print("Debes convertir tus cadenas a enteros en el codigo")
+# except ValueError:
+#     print("Introduce un numero correcto")
+# except Exception as error:
+#     print(type(error))
+#     print(f"Ha ocurrido un error: {type(error).__name__}")
+
+#Excepciones personalizadas o lanzar excepcion
 try:
-    numero = int(input("dime el numero para elevarlo al cuadrado: "))
-    print("El cuadrado es "+ str(numero*numero))
-except TypeError:
-    print("Debes convertir tus cadenas a enteros en el codigo")
+    nombre = input("Introduce el nombre: ")
+    edad = int(input("Introduce la edad: "))
+
+    if edad < 5 or edad > 110:
+        raise ValueError("La edad introducida no es real")
+    elif len(nombre) <= 1:
+        raise ValueError("El nombre no esta completo")
+    else:
+        print(f"Bienvenido al Master en Python {nombre}")
 except ValueError:
-    print("Introduce un numero correcto")
-except Exception as error:
-    print(type(error))
-    print(f"Ha ocurrido un error: {type(error).__name__}")
+    print("Introduce los datos correctamente")
+except Exception as e:
+    print(f"Existe un error: {e}")
